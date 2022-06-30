a0225364cbd856e10978f25dccbcd0e18704b8b1
yskim <rdt2359@gmail.com>
2022-06-30 21:14:44 +0900

    Change Main class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
