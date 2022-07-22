77c2cd806df2d16494aa935a4fe29e21161833b0
yskim <rdt2359@gmail.com>
2022-07-22 17:30:54 +0900

    Solve BJ17219

---
+++ src/bj/BJ17219_비밀번호_찾기.java
@@ -0,0 +1,29 @@
+package bj;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BJ17219_비밀번호_찾기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashMap<String, String> map = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			map.put(st.nextToken(), st.nextToken());
+		}
+		for (int i = 0; i < M; i++) {
+			sb.append(map.get(br.readLine())).append("\n");
+		}
+		System.out.println(sb);
+
+	}
+}
